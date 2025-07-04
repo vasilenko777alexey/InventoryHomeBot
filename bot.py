e758ef8e55ecf2d1100e04124ef4d09789ef6539
vasilenko777alexey <vasilenko777alexey@gmail.com>
2025-07-04 14:11:32 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,46 @@
-import telebot
-from os import environ
+#import telebot
+#from os import environ
 
-bot = telebot.TeleBot(environ['TELEGRAM_TOKEN'])
+import os
+import logging
+from flask import Flask, request
+from telegram import Update, Bot
+from telegram.ext import Dispatcher, CommandHandler
 
-bot_text = '''
-Howdy, how are you doing?
-Source code on https://glitch.com/~{}
-'''
-#.format(environ['PROJECT_NAME'])
+# Вставьте сюда ваш токен бота от BotFather
+#TOKEN = 'ВАШ_ТОКЕН_ЗДЕСЬ'
+TOKEN = environ['TELEGRAM_TOKEN']
+app = Flask(__name__)
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, bot_text)
-from time import sleep
-sleep(2)
-#bot.set_webhook("https://{}.glitch.me/{}".format(environ['PROJECT_NAME'], environ['TELEGRAM_TOKEN']))
+# Инициализация бота и диспетчера
+bot = Bot(token=TOKEN)
+dispatcher = Dispatcher(bot=bot, update_queue=None, use_context=True)
+
+# Логирование для отладки
+logging.basicConfig(level=logging.INFO)
+
+# Обработчик команды /start
+def start(update: Update, context):
+    update.message.reply_text('Привет!')
+
+# Регистрируем обработчик команды /start
+dispatcher.add_handler(CommandHandler('start', start))
+
+# Фейковый HTTP-сервер для обхода автоотключения Render
+@app.route('/', methods=['GET'])
+def index():
+    return 'Бот запущен!'
+
+# Обработка входящих обновлений через webhook
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        update = Update.de_json(request.get_json(force=True), bot)
+        dispatcher.process_update(update)
+    return 'ok'
+
+if __name__ == '__main__':
+    # Запускаем сервер на порту 8080 (Render использует этот порт по умолчанию)
+    port = int(os.environ.get('PORT', 10000))
+	#port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)

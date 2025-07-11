08fdcceb25bb32f6faf2f1634f03c71fb5b6d8f1
vasilenko777alexey <vasilenko777alexey@gmail.com>
2025-07-11 16:11:45 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -114,7 +114,7 @@ async def excel(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def main():
     app = Application.builder().token(TOKEN).updater(None).write_timeout(30).read_timeout(30).build()
     
-    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), echo)) # Обработчик всех текстовых сообщений, кроме команд
+    app.add_handler(MessageHandler(filters.ALL, echo)) # Обработчик всех текстовых сообщений, кроме команд
     #app.add_handler(MessageHandler(filters.TEXT, echo)) # Обработчик всех текстовых сообщений, кроме команд
     #app.add_handler(MessageHandler(def_text, content_types=['text']))
     app.add_handler(CommandHandler('start', start)) 
